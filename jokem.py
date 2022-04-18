7e6c8abf82ff002f0e6e89e2d2e06c924359bdca
nsx07 <felipeaugustodluz@hotmail.com>
2022-04-18 13:07:10 -0300

    Revert "lizard and spock feature"
    This reverts commit 409dd964dacac1c9a16501edf3930741d961e726.

--- jokem.py
+++ jokem.py
@@ -12,14 +12,14 @@ while exit != 1:
     modo = int(input("Digite o modo de jogo que gostaria: "))
 #---Humano_vs_Humano---------------------------------------------------------------------------------------------------
     if modo == 1:
-        saida1 = 1
+        print("Você escolheu o modo Humano vs Humano\n")
+        print("   Regras do jogo;\n","1 =>Pedra: papel > pedra > tesoura\n","2 =>Papel: tesoura > papel > pedra\n","3 =>Tesoura: pedra > tesoura > papel\n")
+        print("   Opções de jogada;\n","1 - Pedra\n","2 - Papel\n","3 - Tesoura\n")
+        saida1 = 0
         win1 = 0
         win2 = 0
         tie = 0
-        while saida1 != 0:
-            print("Você escolheu o modo Humano vs Humano\n")
-            print("   Regras do jogo;\n","1 =>Pedra: papel > pedra > tesoura\n","2 =>Papel: tesoura > papel > pedra\n","3 =>Tesoura: pedra > tesoura > papel\n")
-            print("   Opções de jogada;\n","1 - Pedra\n","2 - Papel\n","3 - Tesoura\n")
+        while saida1 != 1:
             jogada1 = int(input("Player 1! Digite aqui o número da sua jogada: "))
             jogada2 = int(input("Player 2! Digite aqui o número da sua jogada: "))
             if (jogada1 == 1 and jogada2 == 3) or (jogada1 == 2 and jogada2 == 1) or (jogada1 == 3 and jogada2 == 2):
@@ -30,7 +30,7 @@ while exit != 1:
                 win2 = win2 + 1
             elif (jogada1 == jogada2 and (jogada1 and jogada2 in {1,2,3})):
                 print("Empate.\n")
-                tie = + 1
+                tie = tie + 1
             elif (jogada1 or jogada2 != {1,2,3}):
                 print("Ação inexistente. Tente novamente com jogadas entre 1 e 3.\n")
             #---Sair_Modo----------------------------------------------------------------------------------------------
@@ -39,7 +39,7 @@ while exit != 1:
                     print("Ação inexistente. Tente novamente com opções entre 0 e 1")
                     sair = int(input("Se deseja sair do modo de jogo, digite '0'. Caso contrário, digite '1': \n"))
             if sair == 0:
-                saida1 = 0
+                break
             elif sair == 1:
                 continue  
         print('As estatísticas foram;\n','O jogador 1 ganhou:',win1,'\n','O jogador 2 ganhou:',win2,'\n','e houveram',tie,'empate(s)\n')
@@ -49,11 +49,11 @@ while exit != 1:
         print("Você escolheu o modo Humano vs PC\n")
         print("   Regras do jogo;\n","1 =>Pedra: papel > pedra > tesoura\n","2 =>Papel: tesoura > papel > pedra\n","3 =>Tesoura: pedra > tesoura > papel\n")
         print("   Opções de jogada;\n","1 - Pedra\n","2 - Papel\n","3 - Tesoura\n")
-        saida2 = 1
+        saida2 = 0
         winPlayer = 0
         winPC = 0
         tie_humanPC = 0
-        while saida2 != 0:
+        while saida2 != 1:
             human = int(input("Player! Digite aqui o número da sua jogada: "))
             pc = randint(1,3)
             print("PC jogou:",pc)
@@ -65,7 +65,7 @@ while exit != 1:
                 winPC = winPC + 1
             elif human == pc and ((human and pc) in {1,2,3}):
                 print("Empate.\n")
-                tie_humanPC = + 1
+                tie_humanPC = tie_humanPC + 1
             elif (human or pc != [1,2,3]):
                 print("Ação inexistente. Tente novamente com jogadas entre 1 e 3.\n")
             #---Sair_Modo----------------------------------------------------------------------------------------------
@@ -81,13 +81,13 @@ while exit != 1:
         print("-------------------------------------------")
 #---Computador_vs_Computador-------------------------------------------------------------------------------------------
     elif modo == 3:
-        saida3 = 1
+        print("Você escolheu o modo PC vs PC\n")
+        print("     Regras do jogo;\n","1 =>Pedra: papel > pedra > tesoura\n","2 =>Papel: tesoura > papel > pedra\n","3 =>Tesoura: pedra > tesoura > papel\n")
+        saida3 = 0
         win_pc1 = 0
         win_pc2 = 0
         tie_pcPc = 0
-        while saida3 != 0:
-            print("Você escolheu o modo PC vs PC\n")
-            print("     Regras do jogo;\n","1 =>Pedra: papel > pedra > tesoura\n","2 =>Papel: tesoura > papel > pedra\n","3 =>Tesoura: pedra > tesoura > papel\n")
+        while saida3 != 1:
             pc1 = randint(1,3)
             pc2 = randint(1,3)
             print("A jogada do PC1 foi:",pc1)
@@ -119,23 +119,23 @@ while exit != 1:
         print("Você escolheu o modo extra - Humano vs PC com adição de mais duas jogadas!!!\n")
         print("     Regras do jogo;\n","1 =>Pedra: papel > spock > |pedra| > tesoura > lagarto\n","2 =>Papel: tesoura > lagarto > |papel| > spock > pedra\n","3 =>Tesoura: spock > pedra > |tesoura| > lagarto > papel\n","4 =>Lagarto: pedra > tesoura > |lagarto| > papel > spock\n","5 =>Spock: lagarto > papel > |spock| > pedra > tesoura\n")
         print("     Opções de jogada;\n","1 - Pedra\n","2 - Papel\n","3 - Tesoura\n","4 - Lagarto\n","5 - Spock\n")
-        saida2 = 1
-        winPlayer = 0
-        winPC = 0
-        tie_humanPC = 0
-        while saida2 != 0:
+        saida_B = 0
+        winPlayer_B = 0
+        winPC_B = 0
+        tie_humanPC_B = 0
+        while saida_B != 1:
             human = int(input("Player! Digite aqui o número da sua jogada: "))
             pc = randint(1,5)
             print("PC jogou:",pc)
             if (human == 1 and (pc == 3 or pc == 4)) or (human == 2 and (pc == 5 or pc == 1)) or (human == 3 and (pc == 4 or pc == 2)) or (human == 4 and (pc == 2 or pc == 5)) or (human == 5 and (pc  == 1 or pc ==3)):
                 print("Player Ganhou!\n")
-                winPlayer = winPlayer + 1
+                winPlayer_B = winPlayer_B + 1
             elif (pc == 1 and (human == 3 or human == 4)) or (pc == 2 and (human == 5 or human == 1)) or (pc == 3 and (human == 4 or human == 2)) or (pc == 4 and (human == 2 or human == 5)) or (pc == 5 and (human  == 1 or human ==3)):
                 print("PC Ganhou!\n")
-                winPC = winPC + 1
+                winPC_B = winPC_B + 1
             elif human == pc and (human and pc) in {1,2,3,4,5}:
                 print("Empate.\n")
-                tie_humanPC = + 1
+                tie_humanPC_B = tie_humanPC_B + 1
             elif (human or pc != [1,2,3,4,5]):
                 print("Ação inexistente. Tente novamente com jogadas entre 1 e 3.\n")
                 continue
@@ -148,12 +148,12 @@ while exit != 1:
                 break
             elif sair == 1:
                 continue                  
-        print('As estatísticas foram;\n','O Player ganhou:',winPlayer,'\n','O PC ganhou:',winPC,'\n','e houveram',tie_humanPC,'empate(s)\n')
+        print('As estatísticas foram;\n','O Player ganhou:',winPlayer_B,'\n','O PC ganhou:',winPC_B,'\n','e houveram',tie_humanPC_B,'empate(s)\n')
         print("-------------------------------------------")
 #---Sair_do_Jogo--------------------------------------------------------------------------------------------------------
     else:
         if modo == 0:
             break
-        elif modo != {1,2,3,4}:
+        elif modo != [1,2,3,4]:
             print("Ação inexistente. Escolha opções entre 0 e 4.")
             continue
